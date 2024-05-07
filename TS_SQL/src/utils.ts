3e32a222b491bec4486a27b77c84de6b6ca31f98
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-07 22:51:05 +0530

    Initialized the pg client

--- TS_SQL/src/utils.ts
+++ TS_SQL/src/utils.ts
@@ -2,5 +2,7 @@ import { Client } from 'pg';
 
 
 export async function getClient(){
-    
+    const client = new Client("postgresql://ronitkhajuria03:TgA1zXUu0Myl@ep-old-tree-a5v313e2.us-east-2.aws.neon.tech/Practice-sql?sslmode=require");
+    await client.connect();
+    return client;
 }
