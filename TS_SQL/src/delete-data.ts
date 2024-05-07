f650eae76397b747722b074b9c558695a3a058ba
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-08 00:25:49 +0530

    SQL query in TS to delete todo

---
+++ TS_SQL/src/delete-data.ts
@@ -0,0 +1,12 @@
+import { getClient } from "./utils";
+
+async function deleteTodo(todoId: number) {
+    const client = await getClient();
+    const query = `
+        DELETE FROM todos WHERE id = $1
+    `;
+    await client.query(query, [todoId]);
+    console.log(`Todo with ID: ${todoId} deeted successfully`);
+}
+
+deleteTodo(1);
